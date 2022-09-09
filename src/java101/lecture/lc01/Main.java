17b79029b8de92b17d4cb8de63566760885849f9
kamil <kamilaydn1997@gmail.com>
2022-09-09 16:55:28 +0300

    ORY-02-test #time 30m kullanıcıdan notları ve ders sayısını alarak not hesaplama programı

--- src/java101/lecture/lc01/Main.java
+++ src/java101/lecture/lc01/Main.java
@@ -1,9 +1,40 @@
 package java101.lecture.lc01;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+
+        int mat,fiz,bio,ing,tur,beden,muz,ders;
+
         System.out.println("Hello world!");
-        int testInt = 5;
-        System.out.println(testInt);
+        Scanner input= new Scanner(System.in);
+        System.out.println("matematik notunu gir");
+            mat=input.nextInt();
+
+        System.out.println("fizik notunu gir");
+        fiz=input.nextInt();
+
+        System.out.println("biology notunu gir");
+        bio=input.nextInt();
+
+        System.out.println("ingilizce notunu gir");
+        ing=input.nextInt();
+
+        System.out.println("turkce notunu gir");
+        tur=input.nextInt();
+
+        System.out.println("beden notunu gir");
+        beden=input.nextInt();
+
+        System.out.println("muzik notunu gir");
+        muz=input.nextInt();
+
+        System.out.println("Kaç dersin var? ");
+        ders=input.nextInt();
+
+        int toplam= mat+fiz+bio+ing+tur+beden+muz;
+        double sonuc= toplam/ders;
+        System.out.println(sonuc);
     }
 }
