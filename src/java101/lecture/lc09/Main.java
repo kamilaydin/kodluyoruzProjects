96948e0d867a41985706cea0155a45f8d2bfaf3c
kamil <kamilaydn1997@gmail.com>
2022-09-13 09:19:47 +0300

    ORY-09 #time 30m Kullanıcı girisi yapan program

---
+++ src/java101/lecture/lc09/Main.java
@@ -0,0 +1,22 @@
+package java101.lecture.lc09;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.print("Kullanıcı adı giriniz:");
+        Scanner input = new Scanner(System.in);
+        String userName = input.nextLine();
+        System.out.print("Parolanızı giriniz:");
+        String password = input.nextLine();
+        if (userName.equals("kamil") && password.equals("aydin"))
+            System.out.println("Basari ile giris yaptiniz");
+        else System.out.println("Kullanıcı adı veya sifre yanlıs tekrar deneyiniz: ");
+
+
+        ;
+
+
+    }
+}
