0168a524f89b1839227b602e4efb11923a75676d
kamilaydin <44448054+kamilaydin@users.noreply.github.com>
2022-09-13 09:43:55 +0300

    Update Main.java

--- src/java101/lecture/lc09/Main.java
+++ src/java101/lecture/lc09/Main.java
@@ -1,5 +1,6 @@
 package java101.lecture.lc09;
 
+import java.util.Objects;
 import java.util.Scanner;
 
 public class Main {
@@ -12,6 +13,30 @@ public class Main {
         String password = input.nextLine();
         if (userName.equals("kamil") && password.equals("aydin"))
             System.out.println("Basari ile giris yaptiniz");
-        else System.out.println("Kullanıcı adı veya sifre yanlıs tekrar deneyiniz: ");
+        else {
+            System.out.println("Kullanıcı adı veya sifre yanlıs sifre yenilemek icin 1 e basın: cıkıs icin 2 ye basın:");
+            int x = input.nextInt();
+
+            switch (x) {
+                case 1:
+                    System.out.print("Yeni sifreniz: ");
+                    String newPass = input.nextLine();
+
+                    if (Objects.equals(password, newPass)) {
+                        System.out.println("Yeni sifreniz eski sifreniz ile aynı olamaz!");
+
+                    } else password = newPass;
+                    break;
+                case 2:
+                    System.out.println("basari ile cikis yaptiniz !");
+                    break;
+                default:
+                    System.out.println("Gecersiz giris tekrar deneyiniz");
+                    break;
+
+            }
+
+
+        }
     }
 }
