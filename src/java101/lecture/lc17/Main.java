9862f1d15baf1106afb613f99362c9c47a6a39cb
kamil <kamilaydn1997@gmail.com>
2022-09-14 15:03:17 +0300

    ORY-17 #30min Artık YIl Hesaplama

---
+++ src/java101/lecture/lc17/Main.java
@@ -0,0 +1,16 @@
+package java101.lecture.lc17;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int yil;
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Yıl Giriniz :  ");
+        yil = input.nextInt();
+if (yil/4==0)
+        System.out.println(yil+"bir artık yıldır");
+else System.out.println(yil+"bir artık yıldır! ");
+    }
+}
