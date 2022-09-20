8e45a4972480635c3db5b456cd476b7da4c84c09
kamil <kamilaydn1997@gmail.com>
2022-09-20 17:03:32 +0300

    ORY-1 OOP-1 #time 30m +this.mat.tNote*(0.2)

---
+++ src/java101/lecture/Oop/lc01/Course.java
@@ -0,0 +1,49 @@
+package java101.lecture.Oop.lc01;
+
+import javax.swing.plaf.ColorUIResource;
+
+public class Course {
+    Teacher courseTeacher;
+    String name;
+    String code;
+    String prefix;
+
+    int tNote;
+    int note;
+
+    public Course(String name,String code,String prefix,int tNote){
+
+        this.name=name;
+        this.code=code;
+        this.prefix=prefix;
+        this.tNote=tNote;
+        this.note=0;
+    }
+    public void addTeacher(Teacher t) {
+        if (this.prefix.equals(t.branch)) {
+            this.courseTeacher = t;
+            System.out.println("İşlem başarılı");
+        } else {
+            System.out.println(t.name + " Akademisyeni bu dersi veremez.");
+        }
+    }
+
+    public void printTeacher() {
+        if (courseTeacher != null) {
+            System.out.println(this.name + " dersinin Akademisyeni : " + courseTeacher.name);
+        } else {
+            System.out.println(this.name + " dersine Akademisyen atanmamıştır.");
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+}
