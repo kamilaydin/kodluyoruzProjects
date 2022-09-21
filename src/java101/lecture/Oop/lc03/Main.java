29720df978b8c6a75ed131a2b2e1413c74c00713
kamil <kamilaydn1997@gmail.com>
2022-09-21 14:16:39 +0300

    ORY-3 OOP-3 #time 30m Maas Hesaplama

---
+++ src/java101/lecture/Oop/lc03/Main.java
@@ -0,0 +1,8 @@
+package java101.lecture.Oop.lc03;
+
+public class Main {
+    public static void main(String[] args) {
+        Employee W1 = new Employee("kemal", 2000, 45, 1985);
+        W1.toStr ();
+    }
+}
